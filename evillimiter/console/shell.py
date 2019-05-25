ab73d5e0a12bf39fe18cc686f561aefd169d3284
leonardus <23728094+leonardus@users.noreply.github.com>
2019-05-24 23:24:44 -0400

    Fix error when binaries are not in /sbin

--- evillimiter/console/shell.py
+++ evillimiter/console/shell.py
@@ -1,6 +1,8 @@
+import os
 import subprocess
+from evillimiter.console.io import IO
 
-from evillimiter.common.globals import DEVNULL
+DEVNULL = open(os.devnull, 'w')
 
 
 def execute(command, root=True):
@@ -9,3 +11,18 @@ def execute(command, root=True):
 
 def execute_suppressed(command, root=True):
     return subprocess.call('sudo ' + command if root else command, shell=True, stdout=DEVNULL, stderr=DEVNULL)
+
+
+def output(command, root=True):
+    return subprocess.check_output('sudo ' + command if root else command, shell=True).decode('utf-8')
+
+
+def output_suppressed(command, root=True):
+    return subprocess.check_output('sudo ' + command if root else command, shell=True, stderr=DEVNULL).decode('utf-8')
+
+
+def locate_bin(name):
+    try:
+        return output_suppressed('which {}'.format(name)).replace('\n', '')
+    except subprocess.CalledProcessError:
+        IO.error('missing util: {}, check your PATH'.format(name))
