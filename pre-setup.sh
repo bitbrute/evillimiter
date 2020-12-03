449cab2c7652a4cc98d6a491a241e4c35cc9d453
StaticPirate <71192104+StaticPirate@users.noreply.github.com>
2020-12-03 08:55:10 -0700

    fixes common errors

---
+++ pre-setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env
+
+sudo apt install python3-pip	#pip3
+sudo apt-get install python3-setuptools #setuptools of python3
+sudo apt-get install python-dev #python3 dev tools
+sudo apt-get install libffi-dev
+sudo apt-get install python3-dev
