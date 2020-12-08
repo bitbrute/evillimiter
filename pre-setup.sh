7b16b74a730909ae6398454fe0f566321bfcd3c9
StaticPirate <71192104+StaticPirate@users.noreply.github.com>
2020-12-08 10:31:11 -0700

    Update pre-setup.sh

--- pre-setup.sh
+++ pre-setup.sh
@@ -1,4 +1,9 @@
+#install python
+sudo apt-get install python
+sudo apt-get install python2
+sudo apt-get install python3
 
+#install other python depenisyes
 sudo apt install python3-pip	#pip3
 sudo apt-get install python3-setuptools #setuptools of python3
 sudo apt-get install python-dev #python3 dev tools
